5d76beef0b648b65f4c1210f8d9e0d5a4d8ada72
Artem Pospeev <pospeev.artem@icloud.com>
2023-11-19 14:27:23 +0300

    added try except block on celery task

--- parserbot/tasks.py
+++ parserbot/tasks.py
@@ -1,11 +1,19 @@
 import asyncio
+import logging
 
 from celery import shared_task
 
 from scrappers.tiktok.tt_async_scrapper import TikTokScrapper
 
 
+logger = logging.getLogger('tiktok_scrapper')
+
 @shared_task
 def parse_tiktok(decoded_link, chat_id):
     scrapper = TikTokScrapper()
-    return asyncio.run(scrapper.run(decoded_link, tg_chat_id=chat_id))
+    try:
+        res = asyncio.run(scrapper.run(decoded_link, tg_chat_id=chat_id))
+    except Exception as e:
+        logger.error(e)
+        res = e
+    return res
