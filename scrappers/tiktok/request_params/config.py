0efe93cf2111335242c3e77efecc548af93c2377
Artem Pospeev <pospeev.artem@icloud.com>
2024-05-01 11:49:58 +0300

    fixed type annotation

--- scrappers/tiktok/request_params/config.py
+++ scrappers/tiktok/request_params/config.py
@@ -10,22 +10,22 @@ class TikTokScrapperConfig:
             ms_token: str,
             x_bogus: str,
             signature: str,
-            cookies: dict[str:str],
-            params: dict[str:str],
-            headers: dict[str:str],
+            cookies: dict[str, str],
+            params: dict[str, str],
+            headers: dict[str, str],
             is_available: bool = True,
     ):
         self.msToken: str = ms_token
         self.X_Bogus: str = x_bogus
         self.signature: str = signature
-        self.cookies: dict[str:str] = cookies | {
+        self.cookies: dict[str, str] = cookies | {
             'msToken': ms_token,
         }
-        self.params: dict[str:str] = params | {
+        self.params: dict[str, str] = params | {
             'msToken': ms_token,
             'X-Bogus': x_bogus,
             '_signature': signature,
         }
-        self.headers: dict[str:str] = headers
+        self.headers: dict[str, str] = headers
         self.is_available: bool = is_available
         SCRAPPER_TIKTOK_SETTINGS.append(self)
